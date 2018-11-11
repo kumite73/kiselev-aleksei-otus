d1c195eb2005310000a08810ff71c4db7337b682
Aleksei Kiselev <kumite73@gmail.com>
2018-11-11 19:40:52 +0300

    Изменние очереди

--- javascript-3/request.js
+++ javascript-3/request.js
@@ -1,51 +1,53 @@
 // Параметры
 // (количество запросов) (async/chain) (url по умолчанию http://127.0.0.1:3000 )
 
-const request = require('request')
+const http = require('http')
 const host = '127.0.0.1'
 const port = 3000
+const results =[]
+const start = new Date() // для вычисления времени ответа
+
+const requestSrv = (url) => new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    let body = ''
+    let r = ''
+
+    http.get(url, (res) => {
+      let bodyChunks = [];
+      res.on('data', function (chunk) {
+        // You can process streamed parts here...
+        bodyChunks.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(bodyChunks)
+        r = 'Request time in ms ' + (new Date() - start) + ' body: ' + body + ' path: ' + res.req.path
+        results.push(r)
+        resolve(r)
+      })
+    })
+  }).then(r => {
+    console.log(r)
+    resolve(r)
+  })
+})
 
 function requestServer(count, type, url) {
-  let req = []
-  let resultsReq = []
-
-  function getHTTP(u) {
-    return new Promise(function (resolve, reject) {
-      let start = new Date(); // для вычисления времени ответа
-      request(u, function (error, response, body) {
-        if (error) {
-          resultsReq.push(error) // записываем ошибку в массив результатов
-          throw error
-        }
-        // записываем ркзультат в массив результатов
-        resultsReq.push('Request time in ms ' + (new Date() - start) + ' body: ' + body + ' path: ' + response.request.path)
-        // завершаем промис (можно передавать результат, тогда в Promise.all можно будет выводить не массив а сразу результат, но не будет ошибок)
-        resolve()
-      });
-    });
-  }
+  const urls = []
 
   for (let i = 0; i < count; i++) {
-    if (typeof url !== 'undefined') {
-      req.push(getHTTP(url + '/' + i))
-    } else {
-      req.push(getHTTP(`http://${host}:${port}/${i}`))
-    }
+    urls.push(url + '/' + i)
   }
 
   if (type === 'async') {
-    Promise.all(req)
-      .then(() => { console.log(resultsReq) })
+    Promise.all(urls.map((u) => requestSrv(u)))
+      .then(res => {
+        console.log(res)
+        console.log(`Время выполнения: ${new Date() - start}`)
+      })
   } else {
-    let chain = Promise.resolve()
-    for (let r of req) {
-      // работает только через перезаписывание переменной, иначе выводится только первый промис
-      chain = chain
-        .then(() => r)
-    }
-    // Получаем результат
-    chain.then(() => console.log(resultsReq))
+    urls.reduce((prev, current) => {
+      return prev.then(() => requestSrv(current))
+    }, Promise.resolve())
   }
-}
+};
 
-requestServer(process.argv[2], process.argv[3], process.argv[4]);
+requestServer(process.argv[2], process.argv[3], process.argv[4] || `http://${host}:${port}`);
--- javascript-3/server.js
+++ javascript-3/server.js
@@ -5,14 +5,15 @@ const port = 3000
 const server = http.createServer((req, res) => {
   setTimeout(function () {
     // выводим в консоль запрос (снижает скорость ответа примерно на 25% на тестовом стенде)
-    console.log(req)
+    console.log(req.method + ': ' + req.url);
+    console.log(req.headers);
 
     // отдаем результат
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain')
     res.end('Hello World\n')
   }
-  , 100);
+  , 1000);
 })
 
 server.listen(port, hostname, () => {
