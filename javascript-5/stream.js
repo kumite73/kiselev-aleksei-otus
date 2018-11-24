cae09f14d63017bb97e5072c8794a6aedef0bd99
Aleksei Kiselev <kumite73@gmail.com>
2018-11-24 20:47:45 +0300

    Fix after rewiew 1

--- javascript-5/stream.js
+++ javascript-5/stream.js
@@ -5,40 +5,55 @@ const { Transform, Readable, Writable } = require('stream');
 // Указываем потокам, что они работают с объектами
 const setOptionsForStreams = { objectMode: true, highWaterMark: 2 }
 
-const rs = new Readable(setOptionsForStreams);
-const ts = new Transform(setOptionsForStreams);
-const ws = new Writable(setOptionsForStreams);
+class myReadable extends Readable {
+  constructor(options) {
+    super(options);
+    this._read = () => {
+      let rnd = Math.floor(Math.random() * 100);
 
-rs._read = () => {
-  let rnd = parseInt((Math.random() * 100), 10);
+      // Заполняем поток значениями
+      clog('Генерируем число rs: ', 'Шаг + 1', rnd);
+      rs.push(rnd + 1);
+      clog('Генерируем число rs: ', 'Шаг + 2', rnd + 1);
+      rs.push(rnd + 2);
+      clog('Генерируем число rs: ', 'Шаг + 3', rnd + 2);
+      rs.push(rnd + 3);
 
-  // Заполняем поток значениями
-  clog('Генерируем число rs: ', 'Шаг + 1', rnd);
-  rs.push(rnd + 1);
-  clog('Генерируем число rs: ', 'Шаг + 2', rnd + 1);
-  rs.push(rnd + 2);
-  clog('Генерируем число rs: ', 'Шаг + 3', rnd + 2);
-  rs.push(rnd + 3);
-
-  // Указываем потоку, что конец чтения
-  rs.push(null);
-};
-
-ts._transform = (chunk, encoding, done) => {
-  let rnd = parseInt((Math.random() * 100), 10);
-
-  clog('Получаем из rs: ', chunk);
-  clog('Генерируем добавочное число: ', rnd);
-
-  done(null, chunk + rnd)
-  // Другая реализация:
-  // ts.push(chunk + rnd);
-  // done();
+      // Указываем потоку, что конец чтения
+      rs.push(null);
+    };
+  }
 }
 
-ws._write = (chunk, enc, next) => {
-  clog('Пришло из потока ts: ', chunk);
-  next();
-};
+class myTransform extends Transform {
+  constructor(options) {
+    super(options);
+    this._transform = (chunk, encoding, done) => {
+      let rnd = Math.floor(Math.random() * 100);
+
+      clog('Получаем из rs: ', chunk);
+      clog('Генерируем добавочное число: ', rnd);
+
+      done(null, chunk + rnd)
+      // Другая реализация:
+      // ts.push(chunk + rnd);
+      // done();
+    };
+  }
+}
+
+class myWritable extends Writable {
+  constructor(options) {
+    super(options);
+    this._write = (chunk, enc, next) => {
+      clog('Пришло из потока ts: ', chunk);
+      next();
+    };
+  }
+}
+
+const rs = new myReadable(setOptionsForStreams);
+const ts = new myTransform(setOptionsForStreams);
+const ws = new myWritable(setOptionsForStreams);
 
 rs.pipe(ts).pipe(ws);
