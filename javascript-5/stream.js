1bbaf547e2f997efbb8b5dc055418cfdb8752b39
Aleksei Kiselev <kumite73@gmail.com>
2018-11-26 15:58:15 +0300

    Fix

--- javascript-5/stream.js
+++ javascript-5/stream.js
@@ -8,52 +8,59 @@ const setOptionsForStreams = { objectMode: true, highWaterMark: 2 }
 class myReadable extends Readable {
   constructor(options) {
     super(options);
-    this._read = () => {
-      let rnd = Math.floor(Math.random() * 100);
-
-      // Заполняем поток значениями
-      clog('Генерируем число rs: ', 'Шаг + 1', rnd);
-      rs.push(rnd + 1);
-      clog('Генерируем число rs: ', 'Шаг + 2', rnd + 1);
-      rs.push(rnd + 2);
-      clog('Генерируем число rs: ', 'Шаг + 3', rnd + 2);
-      rs.push(rnd + 3);
-
-      // Указываем потоку, что конец чтения
-      rs.push(null);
-    };
   }
+
+  _read() {
+    let rnd = Math.floor(Math.random() * 100);
+
+    // Заполняем поток значениями
+    clog('Генерируем число rs: ', rnd);
+    this.push(rnd);
+    // Указываем потоку, что конец чтения
+    this.push(null);
+  };
 }
 
 class myTransform extends Transform {
   constructor(options) {
     super(options);
-    this._transform = (chunk, encoding, done) => {
-      let rnd = Math.floor(Math.random() * 100);
-
-      clog('Получаем из rs: ', chunk);
-      clog('Генерируем добавочное число: ', rnd);
-
-      done(null, chunk + rnd)
-      // Другая реализация:
-      // ts.push(chunk + rnd);
-      // done();
-    };
   }
+
+  _transform(chunk, encoding, done) {
+    let rnd = Math.floor(Math.random() * 100);
+
+    clog('Получаем из rs: ', chunk);
+    clog('Генерируем добавочное число: ', rnd);
+
+    done(null, chunk + rnd)
+    // Другая реализация:
+    // this.push(chunk + rnd);
+    // done();
+  };
 }
 
 class myWritable extends Writable {
   constructor(options) {
     super(options);
-    this._write = (chunk, enc, next) => {
-      clog('Пришло из потока ts: ', chunk);
-      next();
-    };
   }
+
+  _write(chunk, enc, next) {
+    clog('Пришло из потока ts: ', chunk);
+    next();
+  };
 }
 
 const rs = new myReadable(setOptionsForStreams);
 const ts = new myTransform(setOptionsForStreams);
 const ws = new myWritable(setOptionsForStreams);
 
-rs.pipe(ts).pipe(ws);
+// написал для теста. Как можно передать туда setOptionsForStreams?
+const wTest = new Writable({
+  write(chunk, encoding, callback) {
+    clog('Пришло из потока ts: ', chunk);
+  },
+  objectMode: true,
+  highWaterMark: 2
+});
+
+rs.pipe(ts).pipe(wTest);
