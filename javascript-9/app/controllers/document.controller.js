5c23925cd6dc312837dba56b2d98c917651221ad
Aleksei Kiselev <kumite73@gmail.com>
2018-12-07 10:19:38 +0300

    Fix document rss to documents

--- javascript-9/app/controllers/document.controller.js
+++ javascript-9/app/controllers/document.controller.js
@@ -10,14 +10,14 @@ const id = (req) => { return req.params.id }
 
 // Все документы
 exports.index = function (req, res) {
-  mongoModel.all(rss(req))
+  mongoModel.all(document(req))
     .then(result => { ret.successJson(result, res) })
     .catch(error => { ret.errorJson(error, res) })
 };
 
 // Документ по ИД
 exports.getById = function (req, res) {
-  mongoModel.findById(id(req), rss(req))
+  mongoModel.findById(id(req), document(req))
     .then(result => { ret.successJson(result, res) })
     .catch(error => { ret.errorJson(error, res) })
 };
